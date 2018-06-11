c118f4510cafbaa1d97b7af2e75ea68c8201c66f
Markus <endzeit@gmail.com>
2018-06-11 07:48:48 +0200

    Update deploy config

--- deploy/ecosystem.config.js
+++ deploy/ecosystem.config.js
@@ -14,7 +14,7 @@ module.exports = {
     {
       name: CONFIG.serviceName,
       script: 'app.js',
-      cwd: 'deploy/service',
+      cwd: 'service',
       env: {
         NODE_ENV: 'production',
         SERVER_PORT: 80,
@@ -33,7 +33,7 @@ module.exports = {
       ref: 'origin/master',
       repo: 'https://github.com/berstend/hypertunnel.git',
       path: `/home/${CONFIG.deployUser}/${CONFIG.serviceName}`,
-      'post-deploy': 'npm install hypertunnel-server@latest && pm2 reload ecosystem.config.js --env dev',
+      'post-deploy': 'cd deploy && pwd && npm install hypertunnel-server@latest && pm2 reload ecosystem.config.js --env dev',
       env: {
         NODE_ENV: 'development',
         SERVER_DOMAIN: `${CONFIG.hosts.vagrant}.xip.io`
